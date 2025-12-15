0cece4610444288211ea017ea5d5885a24cadc90
Fernando Rojo <fernando.rojo@vercel.com>
2025-12-15 16:11:13 -0500

    blank size works

--- react-native-aix/example/App.tsx
+++ react-native-aix/example/App.tsx
@@ -12,7 +12,7 @@ import { Aix, AixCellView, AixComposer, AixRef } from 'react-native-aix';
 import { callback } from 'react-native-nitro-modules';
 
 const list = Array.from({ length: 10_000 }, (_, index) => ({
-  height: index % 2 === 1 ? 400 * 2 : 180 * 2,
+  height: index % 2 === 1 ? 400 * 0.5 : 180 * 2,
   backgroundColor: index % 2 === 0 ? '#333' : '#222222',
 }));
 
@@ -24,6 +24,10 @@ function App(): React.JSX.Element {
   const shouldScrollToEnd = useRef(false);
 
   useEffect(() => {
+    if (!shouldScrollToEnd.current) {
+      return;
+    }
+    shouldScrollToEnd.current = false;
     aix.current?.scrollToIndexWhenBlankSizeReady(numMessages - 1, true, false);
   }, [numMessages]);
 
