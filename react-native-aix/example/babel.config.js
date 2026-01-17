3f768bde4385099c4cd362bfd0d4b138d44e2b03
Fernando Rojo <fernando.rojo@vercel.com>
2026-01-17 10:58:05 -0500

    fix: builds

--- react-native-aix/example/babel.config.js
+++ react-native-aix/example/babel.config.js
@@ -7,6 +7,7 @@ module.exports = api => {
     presets: ['module:@react-native/babel-preset'],
     plugins: [
       'react-native-worklets/plugin',
+      '@babel/plugin-transform-export-namespace-from',
       [
         'module-resolver',
         {
