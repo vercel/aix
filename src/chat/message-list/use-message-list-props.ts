5290b91878297dcd41052352e3737b252024edcb
Jay Meistrich <jmeistrich@gmail.com>
2026-02-17 17:25:30 +0900

    Upgrade @legendapp/list to v3 API types
    Signed-off-by: Jay Meistrich <jmeistrich@gmail.com>

--- src/chat/message-list/use-message-list-props.ts
+++ src/chat/message-list/use-message-list-props.ts
@@ -1,19 +1,19 @@
-import { useMessageListContext } from './context'
-import { useScrollViewAnimatedProps } from './use-scrollview-animated-props'
-import type { AnimatedLegendList } from '@legendapp/list/reanimated'
+import { LegendListRef } from "@legendapp/list";
+import { useMessageListContext } from "./context";
+import { useScrollViewAnimatedProps } from "./use-scrollview-animated-props";
+import type { AnimatedLegendList } from "@legendapp/list/reanimated";
 
 export function useMessageListProps({
   bottomInsetPadding,
 }: {
-  bottomInsetPadding?: number
+  bottomInsetPadding?: number;
 } = {}) {
-  const { listRef, onContentSizeChange, onScroll } = useMessageListContext()
+  const { listRef, onContentSizeChange, onScroll } = useMessageListContext();
   return {
     animatedProps: useScrollViewAnimatedProps({ bottomInsetPadding }),
-    ref: listRef,
+    ref: listRef as React.RefObject<LegendListRef>,
     onContentSizeChange,
-    enableAverages: false,
     onScroll,
     maintainVisibleContentPosition: true,
-  } satisfies Partial<React.ComponentPropsWithRef<typeof AnimatedLegendList>>
+  } satisfies Partial<React.ComponentPropsWithRef<typeof AnimatedLegendList>>;
 }
