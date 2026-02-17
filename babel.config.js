138ae69231005fdcfda1e2aa4b9f3e93787b56d6
Jay Meistrich <jmeistrich@gmail.com>
2026-02-17 17:49:19 +0900

    Configure Worklets Babel plugin in app and package
    Signed-off-by: Jay Meistrich <jmeistrich@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    plugins: ['babel-plugin-react-compiler'],
+    plugins: ['babel-plugin-react-compiler', 'react-native-worklets/plugin'],
     presets: ['babel-preset-expo'],
   }
 }
